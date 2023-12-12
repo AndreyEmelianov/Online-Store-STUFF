035c1af26a5164626110bb8c7ae5819b84fd7c10
Andrey Emelianov <621077@mail.ru>
2023-12-12 18:38:47 +0300

    [feat(components)]: реализовал структуру компонента Sidebar и добавил стили

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,32 @@
+import { NavLink } from 'react-router-dom';
+
 import styles from '../../styles/Sidebar.module.css';
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <section className={styles.sidebar}>
+      <div className={styles.title}>CATEGORIES</div>
+      <nav>
+        <ul className={styles.menu}>
+          <li>
+            <NavLink to={`categories/${1}`}>Категория</NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <div className={styles.footer}>
+        <a className={styles.link} href="/help" target="_blank">
+          Help
+        </a>
+        <a
+          className={styles.link}
+          href="/terms"
+          style={{ textDecoration: 'underline' }}
+          target="_blank">
+          Terms & Conditions
+        </a>
+      </div>
+    </section>
+  );
 };
 export default Sidebar;
